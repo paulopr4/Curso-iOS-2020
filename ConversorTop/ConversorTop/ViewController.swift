f005cd68cb8d1e1c3c1d010021170a9990647c5c
Paulo Ricardo <paulo.pr4@hotmail.com>
2020-05-29 16:48:17 -0300

    novos arquivo3

--- ConversorTop/ConversorTop/ViewController.swift
+++ ConversorTop/ConversorTop/ViewController.swift
@@ -9,12 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UITextField!
+    @IBOutlet weak var lbResultUnit: UIButton!
+    @IBOutlet weak var lbUnit: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showNext(_ sender: UIButton) {
+        switch lbUnit.text! {
+        case "Temperatura":
+            lbUnit.text = "Peso"
+            btUnit1.setTitle("Kilograma", for: .normal)
+            btUnit2.setTitle("Libra", for: .normal)
+        case "Peso":
+            lbUnit.text = "Moeda"
+            btUnit1.setTitle("Real", for: .normal)
+            btUnit2.setTitle("Dólar", for: .normal)
+        case "Moeda":
+            lbUnit.text = "Distância"
+            btUnit1.setTitle("Metro", for: .normal)
+            btUnit2.setTitle("Kilometro", for: .normal)
+            
+        default:
+             lbUnit.text = "Temperatura"
+             btUnit1.setTitle("Celsius", for: .normal)
+             btUnit2.setTitle("Farenheint", for: .normal)
+        }
+        convert(nil)
+        
+        
+    }
+    @IBAction func convert(_ sender: UIButton?) {
+        if let sender =  sender {
+            if sender == btUnit1  {
+                btUnit2.alpha = 0.5
+            } else {
+                btUnit1.alpha = 0.5
+            }
+            sender.alpha =  1.0
+            
+        }
+        switch lbUnit.text! {
+        case "Temperatura":
+            calcTemperature()
+        case "Peso":
+            calcWeight()
+        default:
+            calcDistance()
+        }
+        
+    }
+    
+    func calcTemperature() {
+        guard let temperature = Double(tfValue.text!) else {return}
+        if btUnit1.alpha == 1.0 {
+            lbResultUnit.text = "Farenheint"
+            lbResult.text = String(temperature * 1.8 + 32.0)
+           
+           
+        }
+        
+        
+    }
+    func calcWeight() {
+        
+    }
+    
+    func calcCurrency() {
+        
+    }
+    
+    func calcDistance() {
+        
+    }
+    
 
 }
 
